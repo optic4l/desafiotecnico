b44b8e479fc3c966faf7fa852a9e9f79c7efc315
anymal <lksdro01@gmail.com>
2024-04-14 18:28:36 -0400

    creacion del componente para la manejar la paginacion

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,13 +1,16 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import PaginationFooter from "./components/paginationFooter";
 
 function App() {
   const [features, setFeatures] = useState([]);
   const [pagination, setPagination] = useState({});
+  const [page, setPage] = useState(1);
+
   const getFeatures = async () => {
     try {
       const resp = await axios.get(
-        "http://127.0.0.1:3000/api/features?page=1&per_page=2"
+        `http://127.0.0.1:3000/api/features?page=${page}&per_page=10`
       );
       setFeatures(resp.data.data);
       setPagination(resp.data.pagination);
@@ -18,10 +21,11 @@ function App() {
 
   useEffect(() => {
     getFeatures();
-  }, []);
+  }, [page]);
 
   console.log("Features: ", features);
   console.log("Pagination: ", pagination);
+  console.log("page", page);
   return (
     <div className="container flex flex-col justify-center items-center p-5">
       <h1 className="text-3xl font-bold underline">Listado de Features</h1>
@@ -53,6 +57,15 @@ function App() {
               </tr>
             ))}
           </tbody>
+          <tfoot>
+            <PaginationFooter
+              actual={pagination.current_page}
+              limiteInferior={pagination.current_page * 10 - 9}
+              limiteSuperior={pagination.current_page * 10}
+              total={pagination.total}
+              setActualCallback={setPage}
+            />
+          </tfoot>
         </table>
       </div>
     </div>
---
+++ front/src/components/paginationFooter.jsx
@@ -0,0 +1,83 @@
+export default function PaginationFooter({
+  limiteInferior,
+  limiteSuperior,
+  total,
+  actual,
+  setActualCallback,
+}) {
+  return (
+    <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+      <div className="flex flex-1 justify-between sm:hidden">
+        <button
+          onClick={() => setActualCallback(actual - 1)}
+          className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+        >
+          Anterior
+        </button>
+        <button
+          onClick={() => setActualCallback(actual + 1)}
+          className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+        >
+          Siguiente
+        </button>
+      </div>
+      <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+        <div>
+          <p className="text-sm text-gray-700">
+            Mostrando <span className="font-medium">{limiteInferior}</span> a{" "}
+            <span className="font-medium">{limiteSuperior}</span> de{" "}
+            <span className="font-medium">{total}</span> resultados
+          </p>
+        </div>
+        <div>
+          <nav
+            className="isolate inline-flex -space-x-px rounded-md shadow-sm"
+            aria-label="Pagination"
+          >
+            <a className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
+              <button
+                onClick={() => setActualCallback(actual - 1)}
+                className=""
+              >
+                Anterior
+              </button>
+            </a>
+            {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
+            <a
+              aria-current="page"
+              className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            >
+              {actual - 3}
+            </a>
+            <a className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
+              {actual - 2}
+            </a>
+            <a className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex">
+              {actual - 1}
+            </a>
+            <span className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+              {actual}
+            </span>
+            <a className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex">
+              {actual + 1}
+            </a>
+            <a className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
+              {actual + 2}
+            </a>
+            <a className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
+              {actual + 3}
+            </a>
+            <a className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
+              <button
+                onClick={() => setActualCallback(actual + 1)}
+                className=""
+              >
+                Siguiente
+              </button>
+            </a>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+}
