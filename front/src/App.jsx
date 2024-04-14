ce53592e6c9d3341d18b723511117fb79f58ea1d
anymal <lksdro01@gmail.com>
2024-04-14 17:46:09 -0400

    creacion de la vista para el listado de features: peticion inicial

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [features, setFeatures] = useState([]);
+  const [pagination, setPagination] = useState({});
+  const getFeatures = async () => {
+    try {
+      const resp = await axios.get(
+        "http://127.0.0.1:3000/api/features?page=1&per_page=2"
+      );
+      setFeatures(resp.data.data);
+      setPagination(resp.data.pagination);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
+  useEffect(() => {
+    getFeatures();
+  }, []);
+
+  console.log("Features: ", features);
+  console.log("Pagination: ", pagination);
   return (
-    <>
+    <div className="container flex flex-col justify-center items-center p-5">
+      <h1 className="text-3xl font-bold underline">Listado de Features</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <table>
+          <thead>
+            <tr>
+              <td>Titulo</td>
+              <td>Lugar</td>
+              <td>Magnitud</td>
+              <td>Tipo Magnitud</td>
+              <td>Coordenadas</td>
+              <td>URL</td>
+            </tr>
+          </thead>
+          <tbody>
+            {features?.map((feature) => (
+              <tr key={feature.id}>
+                <td>{feature.attributes.title}</td>
+                <td>{feature.attributes.place}</td>
+                <td>{feature.attributes.magnitude}</td>
+                <td>{feature.attributes.magType}</td>
+                <td>
+                  {feature.attributes.coordinates.latitude}
+                  {","}
+                  {feature.attributes.coordinates.longitude}
+                </td>
+                <td>{feature.attributes.links.external_url}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
