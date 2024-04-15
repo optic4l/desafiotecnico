ccc664a01188d6ccbfd4b6aa3faaf08c77f4e040
anymal <lksdro01@gmail.com>
2024-04-14 22:01:18 -0400

    implementacion del filtrado por mag_type

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,36 +1,50 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Filters from "./components/Filters";
 import PaginationFooter from "./components/paginationFooter";
 
 function App() {
   const [features, setFeatures] = useState([]);
   const [pagination, setPagination] = useState({});
   const [page, setPage] = useState(1);
+  const [filters, setFilters] = useState([]);
 
   const getFeatures = async () => {
     try {
-      const resp = await axios.get(
-        `http://127.0.0.1:3000/api/features?page=${page}&per_page=10`
+      let filtersParams = "";
+      if (filters.length > 0) {
+        filters.forEach((filter) => {
+          filtersParams += `&mag_type[]=${filter}`;
+        });
+      }
+      const { data } = await axios.get(
+        `http://127.0.0.1:3000/api/features?page=${page}&per_page=10${filtersParams}`
       );
-      setFeatures(resp.data.data);
-      setPagination(resp.data.pagination);
+      setFeatures(data.data);
+      setPagination(data.pagination);
     } catch (error) {
       console.error(error);
     }
   };
 
+  const handleSetFilters = (filters) => {
+    setFilters(filters);
+  };
+
   useEffect(() => {
     getFeatures();
-  }, [page]);
+  }, [page, filters]);
 
   console.log("Features: ", features);
   console.log("Pagination: ", pagination);
   console.log("page", page);
+  console.log("filters: ", filters);
   return (
-    <div className="container flex flex-col justify-center items-center p-5">
+    <div className="container flex flex-col justify-center items-center p-5 w-full">
       <h1 className="text-3xl font-bold underline">Listado de Features</h1>
-      <div>
+      <div className="container p-5">
+        <Filters setFiltersCallback={handleSetFilters} />
         <table>
           <thead>
             <tr>
@@ -55,9 +69,11 @@ function App() {
                   {feature.attributes.coordinates.longitude}
                 </td>
                 <td>{feature.attributes.links.external_url}</td>
-                <Link to={`${feature.id}`} state={feature}>
-                  Detalles
-                </Link>
+                <td>
+                  <Link to={`${feature.id}`} state={feature}>
+                    Detalles
+                  </Link>
+                </td>
               </tr>
             ))}
           </tbody>
